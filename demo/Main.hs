107a77b0e1ee6eed5b984771aa82515ed8cfd3e1
yksym <yksym@none>
2018-08-08 08:16:52 +0900

    looks easier?

--- demo/Main.hs
+++ demo/Main.hs
@@ -7,7 +7,6 @@ module Main where
 
 import PrimalSpec
 import Control.Lens
-import Text.Printf (printf)
 
 
 data VMState = VMState {
@@ -24,10 +23,6 @@ data VMEvent
 
 makeLenses ''VMState
 
-
-satisfyInvariant :: VMState -> Bool
-satisfyInvariant s  = s ^. vmCoin <= 10
-
 type Process = ProcExp VMEvent
 
 entry :: UseSuchThat => Process
@@ -40,24 +35,28 @@ entry = vm VMState {
 complexCalc :: UseSuchThat => VMState -> Int
 complexCalc s = s_t_ "what is n?" (\n -> (n + (s ^. vmCoin)) `mod` 4 == 1)
 
+
+
 vm :: UseSuchThat => VMState -> Process
-vm s = printf "vmloop: %s" (show s) *!* if
-  | not (satisfyInvariant s) ->
-    Stop
-  | s ^. juice == 0 ->
-    Fill ?-> \n -> vm (s &~ do { vmCoin .= complexCalc s; juice += n; })
-  | s ^. juice == 1 ->
-    Coin --> Juice --> vm (s &~ do { vmCoin += 1; juice -= 1; })
-    |=| Fill ?-> \n -> vm (s &~ do { vmCoin .= complexCalc s; juice += n; })
-  | s ^. juice >  1 ->
-    Coin --> Juice --> (vm (s &~ do { vmCoin += 1; juice -= 1; }) |=| Bingo --> Juice --> vm (s &~ do { vmCoin += 1; juice -= 2; }))
-    |=| Fill ?-> \n -> vm (s &~ do { vmCoin .= complexCalc s; juice += n; })
+vm s
+  | s ^. juice == 0           =  Fill ?-> \n -> vm (s &~ do { vmCoin .= complexCalc s; juice += n; })
+
+  | s ^. juice == 1           =  Coin --> Juice --> vm (s &~ do { vmCoin += 1; juice -= 1; })
+                             |=| Fill ?-> \n -> vm (s &~ do { vmCoin .= complexCalc s; juice += n; })
+
+  | s ^. juice >  1           =  Coin --> Juice -->
+                                   (   vm (s &~ do { vmCoin += 1; juice -= 1; })
+                                   |=| Bingo --> Juice --> vm (s &~ do { vmCoin += 1; juice -= 2; })
+                                   )
+                             |=| Fill ?-> \n -> vm (s &~ do { vmCoin .= complexCalc s; juice += n; })
+
+  | otherwise                 = Stop
 
 test :: Process
 test = Coin --> Juice --> Coin --> Juice --> Fill 1 --> Coin --> Juice --> Skip
 
 main :: IO ()
-main = useSuchThat StdInSuchThatImpl $ repl $ entry
+--main = useSuchThat StdInSuchThatImpl $ repl entry
 --main = useSuchThat StdInSuchThatImpl $ repl $ entry <||> test
---main = useSuchThat StdInSuchThatImpl <$> (print $ autoStep $ entry <||> test)
+main = print $ useSuchThat StdInSuchThatImpl $ autoStep $ entry <||> test
 
