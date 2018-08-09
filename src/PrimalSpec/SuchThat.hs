cc981a42331017a928b9ac44a1696650e5cfffd8
yksym <yksym@none>
2018-08-09 23:21:29 +0900

    add hint such that

--- src/PrimalSpec/SuchThat.hs
+++ src/PrimalSpec/SuchThat.hs
@@ -18,24 +18,31 @@ import Data.Reflection (Given, give, given)
 import System.IO.Unsafe (unsafePerformIO)
 --import Data.IORef
 import PrimalSpec.Util
+import Data.IORef
 
 class SuchThat s where
     suchThat :: (Read a) => s -> String -> (a -> Bool) -> a
+    hintSuchThat :: (Show a) => s -> a -> b -> b
 
 data SomeSuchThat = forall s. SuchThat s => SomeSuchThat s
 
 instance SuchThat SomeSuchThat where
     suchThat (SomeSuchThat s) = suchThat s
+    hintSuchThat (SomeSuchThat s) = hintSuchThat s
 
---data IORefSuchThatImpl = IORefSuchThatImpl (IORef String)
+data IORefSuchThatImpl = IORefSuchThatImpl (IORef String)
 
 data StdInSuchThatImpl = StdInSuchThatImpl
 
 instance SuchThat StdInSuchThatImpl where
   suchThat _ q p = unsafePerformIO $ askStdIn q p
+  hintSuchThat _ _ = id
 
---instance SuchThat IORefSuchThatImpl where
---  suchThat (IORefSuchThatImpl ref) q p = unsafePerformIO $ askStringIORef ref q p
+instance SuchThat IORefSuchThatImpl where
+  suchThat (IORefSuchThatImpl ref) q p = unsafePerformIO $ askStringIORef ref q p
+  hintSuchThat (IORefSuchThatImpl ref) q p = unsafePerformIO $ do
+   writeIORef ref (show q)
+   return p
 
 
 type UseSuchThat = Given SomeSuchThat
@@ -45,16 +52,4 @@ s_t_ = suchThat (given :: SomeSuchThat)
 
 useSuchThat :: SuchThat s => s -> (Given SomeSuchThat => r) -> r
 useSuchThat = give . SomeSuchThat
-
---type UseStringIORef = Given (IORef String)
---
---s_t_solve :: UseStringIORef => String -> a -> a
---s_t_solve  s a = unsafePerformIO $ do
---    writeIORef given s
---    return a
---
---(|?|) :: UseStringIORef => String -> a -> a
---(|?|) = s_t_solve
---
---infixr 0  |?|
 
