a4bb0e492f067a03ee8ad2d5472624964d9b1d60
yksym <yksym@none>
2018-08-25 23:12:22 +0900

    comment out interrupt

--- src/PrimalSpec/ProcExp.hs
+++ src/PrimalSpec/ProcExp.hs
@@ -6,7 +6,7 @@ module PrimalSpec.ProcExp
 , (&!->)
 , (*?*)
 , (|=|)
-, (PrimalSpec.ProcExp.<|>)
+-- , (PrimalSpec.ProcExp.<|>)
 , (<||>)
 , (>>>)
 , skipExists
@@ -33,7 +33,7 @@ data ProcExp ev
     | Prefix ev (ProcExp ev)
     | forall a. Recv (a -> ev) (a -> ProcExp ev)
     | ExternalChoise (ProcExp ev) (ProcExp ev)
-    | Interrupt (ProcExp ev) (ProcExp ev)
+    -- | Interrupt (ProcExp ev) (ProcExp ev)
     | Sequential (ProcExp ev) (ProcExp ev)
     | Parallel (ProcExp ev) (ProcExp ev)
     | forall a. (Show a) => DebugShow a (ProcExp ev)
@@ -45,7 +45,7 @@ instance forall ev. (Show ev, Data ev) => Show (ProcExp ev) where
         go depth pre (Prefix ev _)          = indent depth <> pre <> show ev <> " -> ..." <> endl
         go depth pre (Recv evc _)           = indent depth <> pre <> constrNameOf evc <> "?x -> ..." <> endl
         go depth pre (ExternalChoise p1 p2) = indent depth <> pre <> "|=|" <> endl <> mconcat [go (depth+1) "" p | p <- [p1, p2]]
-        go depth pre (Interrupt p1 p2)      = indent depth <> pre <> "<|>" <> endl <> go (depth+1) "" p1 <> go (depth+1) "" p2
+        --go depth pre (Interrupt p1 p2)      = indent depth <> pre <> "<|>" <> endl <> go (depth+1) "" p1 <> go (depth+1) "" p2
         go depth pre (Sequential p1 p2)     = indent depth <> pre <> " ; " <> endl <> go (depth+1) "" p1 <> go (depth+1) "" p2
         go depth pre (Parallel p1 p2)       = indent depth <> pre <> "<||>" <> endl <> go (depth+1) "" p1 <> go (depth+1) "" p2
         go depth pre (DebugShow s p)        = indent depth <> pre <> s' <> endl <> go depth pre p
@@ -76,8 +76,8 @@ infixr 4  -->, ?->, &->, &!->, *?*
 (|=|) :: ProcExp ev -> ProcExp ev -> ProcExp ev
 (|=|) = ExternalChoise
 
-(<|>) :: ProcExp ev -> ProcExp ev -> ProcExp ev
-(<|>) = Interrupt
+--(<|>) :: ProcExp ev -> ProcExp ev -> ProcExp ev
+--(<|>) = Interrupt
 
 (<||>) :: ProcExp ev -> ProcExp ev -> ProcExp ev
 (<||>) = Parallel
@@ -85,13 +85,13 @@ infixr 4  -->, ?->, &->, &!->, *?*
 (>>>) :: ProcExp ev -> ProcExp ev -> ProcExp ev
 (>>>) = Sequential
 
-infixl 3 >>>,  |=|, <|>, <||>
+infixl 3 >>>,  |=|, <||> -- <|>, 
 
 simp :: (Show ev, Data ev) => ProcExp ev -> ProcExp ev
-simp (Interrupt Skip _)              = Skip
-simp (Interrupt Stop _)              = Stop
-simp (Interrupt (Sequential Skip p) _) = simp p
-simp (Interrupt p1 p2)              = Interrupt (simp p1) (simp p2)
+--simp (Interrupt Skip _)              = Skip
+--simp (Interrupt Stop _)              = Stop
+--simp (Interrupt (Sequential Skip p) _) = simp p
+--simp (Interrupt p1 p2)              = Interrupt (simp p1) (simp p2)
 simp (Sequential Skip p)            = simp p
 simp (Sequential Stop _)            = Stop
 simp (Sequential p1 p2)             = Sequential (simp p1) p2
@@ -120,13 +120,13 @@ tryStep (ExternalChoise p1 p2) ev
 tryStep (Sequential Skip p2) ev = tryStep p2 ev
 tryStep (Sequential p1 p2) ev = Sequential <$> tryStep p1 ev <*> Just p2
 --tryStep (InternalChoise p1 p2) ev = tryStep (chooseIC p1 p2) ev
-tryStep (Interrupt p1 p2) ev
-    | isJust p1' && isJust p2' = error "Interrupt for same event!!"
-    | isJust p2' = p2'
-    | otherwise  = Interrupt <$> p1' <*> return p2
-    where
-        p1' = tryStep p1 ev
-        p2' = tryStep p2 ev
+--tryStep (Interrupt p1 p2) ev
+--    | isJust p1' && isJust p2' = error "Interrupt for same event!!"
+--    | isJust p2' = p2'
+--    | otherwise  = Interrupt <$> p1' <*> return p2
+--    where
+--        p1' = tryStep p1 ev
+--        p2' = tryStep p2 ev
 tryStep (Parallel p1 p2) ev = Parallel <$> p1' <*> p2'
     where
         p1'  = tryStep p1 ev
@@ -137,7 +137,7 @@ tryStep (DebugShow _ p) ev =  tryStep p ev
 candidates :: (Ord ev) => ProcExp ev -> S.Set ev
 candidates (Prefix ev _) = S.singleton ev
 candidates (ExternalChoise p1 p2) = candidates p1 `S.union` candidates p2
-candidates (Interrupt p1 p2) = candidates p1 `S.union` candidates p2
+--candidates (Interrupt p1 p2) = candidates p1 `S.union` candidates p2
 --candidates (Parallel p1 p2) = candidates p1 `S.intersection` candidates p2
 candidates (Parallel p1 p2) = candidates p1 `S.union` candidates p2
 candidates _ = S.empty
